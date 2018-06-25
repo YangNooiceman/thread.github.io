3502a573fdbcdc6610cdcd16a651f0903f5d4094
zhongjialing <1229036403@qq.com>
2018-06-25 16:51:54 +0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const htmlCol = document.querySelector('.html');
+
+htmlCol.addEventListener('click', ()=> {
+    window.open('../子页面/zi.html');
+})
+
+
+const cssCol = document.querySelector('.css');
+
+cssCol.addEventListener('click', ()=> {
+    window.open('../子页面/zi.html');
+})
+
+
+const javaCol = document.querySelector('.javascript');
+
+javaCol.addEventListener('click', ()=> {
+    window.open('../子页面/zi.html');
+})
